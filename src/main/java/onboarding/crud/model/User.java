218bfe2958fa8132b0124e5f32dab89ea2d3f451
kimjihoon3106 <s24021@gsm.hs.kr>
2024-06-04 22:50:49 +0900

    Feat : Model User and UserRepository

---
+++ src/main/java/onboarding/crud/model/User.java
@@ -0,0 +1,43 @@
+package onboarding.crud.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/onboarding/crud/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package onboarding.crud.repository;
+
+import onboarding.crud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    User findByUsername(String username);
+}
