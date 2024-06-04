0f8d071e74d3330d79ea269b1eca2a7628660504
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-04 21:06:18 +0900

    Service & Repository template

---
+++ src/main/java/onboarding/crud/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package onboarding.crud.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+}
---
+++ src/main/java/onboarding/crud/service/UserService.java
@@ -0,0 +1,12 @@
+package onboarding.crud.service;
+
+import lombok.AllArgsConstructor;
+import onboarding.crud.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    public UserRepository userRepository;
+
+}
