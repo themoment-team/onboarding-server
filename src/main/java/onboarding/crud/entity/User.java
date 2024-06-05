a2caeb47d084d3dd3645971631b8d635a5519cd4
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-05 13:52:54 +0900

    Use lombok getter & setter

--- src/main/java/onboarding/crud/entity/User.java
+++ src/main/java/onboarding/crud/entity/User.java
@@ -1,9 +1,13 @@
 package onboarding.crud.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -16,28 +20,4 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
