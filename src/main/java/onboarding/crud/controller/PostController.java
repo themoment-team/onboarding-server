dad953005044f78f5f747edd899b5c01d6f29fe7
kimjihoon3106 <s24021@gsm.hs.kr>
2024-06-06 14:18:11 +0900

    Feat : PostController and getAllPosts Method

---
+++ src/main/java/onboarding/crud/controller/PostController.java
@@ -0,0 +1,23 @@
+package onboarding.crud.controller;
+
+import onboarding.crud.entity.Post;
+import onboarding.crud.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+}
