2e2e92706b4803c3cc678ec818a0c4c324be7ce3
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-06 21:45:19 +0900

    correct jpa field name and return type

--- src/main/java/onboarding/crud/repository/UserRepository.java
+++ src/main/java/onboarding/crud/repository/UserRepository.java
@@ -4,8 +4,10 @@ import onboarding.crud.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    User findByUsername(String username);
-    User findById(long id);
+    Optional<User> findByName(String name);
+    Optional<User> findById(long id);
 }
