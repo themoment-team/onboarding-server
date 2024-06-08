488913feb1e655714b067dee07d5ad1cab5fbcde
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-08 16:42:07 +0900

    make a new session when user login

--- src/main/java/onboarding/crud/controller/UserController.java
+++ src/main/java/onboarding/crud/controller/UserController.java
@@ -1,6 +1,7 @@
 package onboarding.crud.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import onboarding.crud.entity.User;
 import onboarding.crud.service.UserService;
@@ -44,23 +45,28 @@ public class UserController{
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody Map<String, String> loginData) {
+    public ResponseEntity<String> loginUser(@RequestBody Map<String, String> loginData, HttpServletRequest request) {
 
-        try {
-            String name = loginData.get("name");
-            String password = loginData.get("password");
-            Optional<User> user = userService.loginUser(name, password);
-            if (user.isPresent()) {
-                return ResponseEntity.ok("로그인이 성공적으로 완료되었습니다.");
-            } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인에 실패했습니다. 유효한 사용자 이름과 비밀번호를 입력하세요.");
-            }
+        String name;
+        String password;
+        try{
+            name = loginData.get("name");
+            password = loginData.get("password");
         } catch (Exception e) {
-            if(e instanceof NullPointerException || e instanceof ClassCastException)
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("요청 정보가 형식에 맞지 않습니다.");
-            else
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("로그인 중 오류가 발생했습니다.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("요청 정보가 형식에 맞지 않습니다.");
         }
+
+        Optional<User> _user = userService.loginUser(name, password);
+        if(_user.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인에 실패했습니다. 유효한 사용자 이름과 비밀번호를 입력하세요.");
+        }
+        User user = _user.get();
+        //기존세션 만료후 새 세션 발급
+        request.getSession().invalidate();
+        HttpSession session = request.getSession();
+        session.setAttribute("userId",user.getId());
+        session.setMaxInactiveInterval(30*60);
+        return ResponseEntity.ok("로그인이 성공적으로 완료되었습니다.");
     }
 
     @PostMapping("/logout")
