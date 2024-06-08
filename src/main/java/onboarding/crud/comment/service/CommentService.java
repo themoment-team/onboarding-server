9968fcdf4b10034df2cde94573f4316bb6839bf6
kimjihoon3106 <s24021@gsm.hs.kr>
2024-06-08 16:13:18 +0900

    Feat : getCommentsByPostId method in CommentService

--- src/main/java/onboarding/crud/comment/service/CommentService.java
+++ src/main/java/onboarding/crud/comment/service/CommentService.java
@@ -1,9 +1,12 @@
 package onboarding.crud.comment.service;
 
+import onboarding.crud.comment.entity.Comment;
 import onboarding.crud.comment.repository.CommentRepository;
 import onboarding.crud.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentService {
@@ -12,5 +15,9 @@ public class CommentService {
 
     @Autowired
     private PostRepository postRepository;
+
+    public List<Comment> getCommentsByPostId(Long postId) {
+        return commentRepository.findByPostId(postId);
+    }
 
 }
