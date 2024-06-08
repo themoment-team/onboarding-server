b8e97b2024ad593d8a48dcce938ae3d6c598c315
kimjihoon3106 <s24021@gsm.hs.kr>
2024-06-08 16:17:42 +0900

    Feat : getCommentsByPostId method in CommentController

--- src/main/java/onboarding/crud/comment/controller/CommentController.java
+++ src/main/java/onboarding/crud/comment/controller/CommentController.java
@@ -1,14 +1,28 @@
 package onboarding.crud.comment.controller;
 
+import onboarding.crud.comment.entity.Comment;
+import onboarding.crud.comment.service.CommentService;
+import onboarding.crud.post.entity.Post;
 import onboarding.crud.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/posts/{postId}/comments")
 public class CommentController {
 
     @Autowired
-    private PostService postService;
+    private CommentService commentService;
+
+    @GetMapping
+    public List<Comment> getCommentsByPostId(@PathVariable Long postId) {
+        return commentService.getCommentsByPostId(postId);
+    }
+
+
 }
