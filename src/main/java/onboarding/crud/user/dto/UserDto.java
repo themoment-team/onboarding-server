a371e8b3007d030bde96b9c25679039155423659
kimjihoon3106 <s24021@gsm.hs.kr>
2024-06-10 14:11:21 +0900

    User 엔티티에서 id와 name만 반환하는 UserDto 생성

---
+++ src/main/java/onboarding/crud/user/dto/UserDto.java
@@ -0,0 +1,13 @@
+package onboarding.crud.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private long id;
+    private String name;
+}
