733d8133c6c5cfb4b6f44c7fa01042a1ed239d16
kimjihoon3106 <s24021@gsm.hs.kr>
2024-06-16 00:19:10 +0900

    Fix : "delete never used findByName"

--- src/main/java/onboarding/crud/user/repository/UserRepository.java
+++ src/main/java/onboarding/crud/user/repository/UserRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByName(String name);
 
     Optional<User> findById(long id);
 
