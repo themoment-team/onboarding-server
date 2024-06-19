e2896c0c116e255e1829995609cd22b69d848b88
kimjihoon3106 <s24021@gsm.hs.kr>
2024-06-19 22:16:58 +0900

    Feat : "PostWithCommentsDto in PostService"

--- src/main/java/onboarding/crud/post/service/PostService.java
+++ src/main/java/onboarding/crud/post/service/PostService.java
@@ -1,13 +1,19 @@
 package onboarding.crud.post.service;
 
 import lombok.AllArgsConstructor;
+import onboarding.crud.comment.dto.CommentDto;
+import onboarding.crud.comment.entity.Comment;
+import onboarding.crud.comment.repository.CommentRepository;
 import onboarding.crud.post.dto.CreatePostDto;
 import onboarding.crud.post.dto.PostDto;
+import onboarding.crud.post.dto.PostWithCommentsDto;
 import onboarding.crud.post.dto.UpdatePostDto;
 import onboarding.crud.post.entity.Post;
 import onboarding.crud.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +26,9 @@ public class PostService {
     @Autowired
     public PostRepository postRepository;
 
+    @Autowired
+    public CommentRepository commentRepository;
+
     public PostDto writePost(CreatePostDto createPostDto) {
         Post post = new Post();
         post.setTitle(createPostDto.getTitle());
@@ -29,6 +38,31 @@ public class PostService {
         Post savedPost = postRepository.save(post);
         return convertToDto(savedPost);
     }
+
+    public PostWithCommentsDto getPostWithCommentsById(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found with id: " + postId));
+
+        // Increase view count
+        post.setViewCount(post.getViewCount() + 1);
+        postRepository.save(post);
+
+        // Convert post to DTO
+        PostDto postDto = convertToDto(post);
+
+        // Get comments for the post and convert to DTOs
+        List<CommentDto> comments = commentRepository.findByPostId(postId).stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+
+        // Create PostWithCommentsDto and set postDto and comments
+        PostWithCommentsDto postWithCommentsDto = new PostWithCommentsDto();
+        postWithCommentsDto.setPost(postDto);
+        postWithCommentsDto.setComments(comments);
+
+        return postWithCommentsDto;
+    }
+
 
     public Optional<PostDto> getPostById(Long id) {
         return postRepository.findById(id)
@@ -96,5 +130,12 @@ public class PostService {
         postDto.setViewCount(post.getViewCount());
         postDto.setLikes(post.getLikes());
         return postDto;
+    }
+
+    private CommentDto convertToDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setContent(comment.getContent());
+        return commentDto;
     }
 }
